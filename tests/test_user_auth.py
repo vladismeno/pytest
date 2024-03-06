0aca96abbfed9fad8fbbb871b084bfff3740067d
vladismeno <vladismeno@gmail.com>
2024-03-05 21:55:04 -0800

    Lesson 27: BaseCase

---
+++ tests/test_user_auth.py
@@ -0,0 +1,52 @@
+import requests
+import pytest
+
+from lib.base_case import BaseCase
+
+
+class TestUserAuth(BaseCase):
+    exclude_params = [
+        "no_cookie",
+        "no_token"
+    ]
+
+    def setup_method(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1,"user_id")
+
+    def test_auth_user(self):
+        response2 = requests.get(
+            "https://playground.learnqa.ru/api/user/auth",
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
+        )
+
+        self.user_id_from_check_method = self.get_json_value(response2, "user_id")
+
+        assert self.user_id_from_auth_method == self.user_id_from_check_method, "User id from auth method is not equal to user id from check method"
+
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+
+        if condition == "no_cookie":
+            response = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                headers={"x-csrf-token": self.token}
+            )
+        else:
+            response = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                cookies={"auth_sid": self.auth_sid}
+            )
+
+        self.user_id_from_check_method = self.get_json_value(response, "user_id")
+
+        assert self.user_id_from_check_method == 0, f"User is authorized with condition {condition}"
