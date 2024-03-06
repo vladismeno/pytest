b62ef602d0fdf50180c94f2d3420bea1dd882018
vladismeno <vladismeno@gmail.com>
2024-03-05 18:26:27 -0800

    Lesson 23: Параметризированный тест

--- test_first_api.py
+++ test_first_api.py
@@ -1,10 +1,17 @@
 import requests
+import pytest
 
 
 class TestFirstAPI:
-    def test_hello_call(self):
+    names = [
+        ("Vitalii"),
+        ("Arseniy"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Vitalii'
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -14,6 +21,10 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in the response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
+
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Actual text in the response is not correct"
